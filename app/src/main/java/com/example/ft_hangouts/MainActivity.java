dcf25aa58ba80e17b542dea0bd3544b1c9e9d7af
acosi <cosianthony@gmail.com>
2026-04-21 08:55:40 +0200

    Added a button

--- app/src/main/java/com/example/ft_hangouts/MainActivity.java
+++ app/src/main/java/com/example/ft_hangouts/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.ft_hangouts;
 
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.View;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton addNewContact = findViewById(R.id.add_button);
+        addNewContact.setOnClickListener(v -> {
+            Log.d("MainActivity", "Add contact");
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
+
+
 }
